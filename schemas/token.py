79b69626e38a529468e27f16da7cee75411010ae
Le Thanh Vinh <thanhvinh2400@gmail.com>
2025-06-17 09:02:44 +0700

    hot fix - delete some class of token

--- schemas/token.py
+++ schemas/token.py
@@ -5,15 +5,3 @@ class Token(BaseModel):
     token_type: str = "bearer"
     expires_in: int
     
-
-class TokenData(BaseModel):
-    username:str | None = None
-    user_id: str | None = None
-
-class RefreshTokenRequest(BaseModel):
-    refresh_token: str
-
-class AccessTokenResponse(BaseModel):
-    access_token: str
-    token_type: str = "bearer"
-    expires_in: int 
